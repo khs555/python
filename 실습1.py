1c5abe61bd607306da53332a58313a1193cdc8db
khs555 <glm7277@nvaer.com>
2023-01-02 18:53:31 +0900

    정리

---
+++ 실습1.py
@@ -0,0 +1,68 @@
+number = int(input('숫자를 입력해주세요 >'))
+print(number+10)
+
+
+food = input('좋아하는 음식을 입력해주세요 >')
+print(food)
+
+
+name = input('이름을 입력해주세요 >')
+year = int(input('태어난 년도를 입력해주세요 >'))
+print('저의 이름은',name+'이고, 올해 나이는', str(2024-year)+ '세 입니다.')
+
+
+string1 = input('첫 번째 문장을 입력해주세요 >')
+string2 = input('두 번째 문장을 입력해주세요 >')
+print(string1 + string2)
+
+
+number2 = int(input('숫자를 입력해주세요 >'))
+number2 = -number2
+print(int(number2))
+
+
+number3 =int(input('첫 번째 정수형 숫자를 입력해주세요 >'))
+number4 =int(input('두 번째 정수형 숫자를 입력해주세요 >'))
+print(int(number3 + number4))
+print(int(number3 - number4))
+print(int(number3 * number4))
+print(int(number3 // number4))
+print(int(number3 % number4))
+
+
+number5 =int(input('첫 번째 정수형 숫자를 입력해주세요 >'))
+number6 =int(input('두 번째 정수형 숫자를 입력해주세요 >'))
+number7 =int(input('세 번째 정수형 숫자를 입력해주세요 >'))
+print(int((number5 + number6 + number7) / 3))
+
+
+number8 =int(input('첫 번째 정수형 숫자를 입력해주세요 >'))
+number9 =int(input('두 번째 정수형 숫자를 입력해주세요 >'))
+number10 =int(input('세 번째 정수형 숫자를 입력해주세요 >'))
+number11 =int(input('네 번째 정수형 숫자를 입력해주세요 >'))
+number12 =int(input('다섯 번째 정수형 숫자를 입력해주세요 >'))
+list =[number8, number9, number10, number11, number12]
+print(input(list))
+
+
+string3 = input('문자열을 입력해주세요 >')
+number13 = int(input('정수형 숫자를 입력해주세요 >'))
+print(input(string3 * number13))
+
+
+sum =0
+number14 =int(input('첫 번째 정수형 숫자를 입력해주세요 >'))
+sum +=number14
+print(sum)
+number15 =int(input('두 번째 정수형 숫자를 입력해주세요 >'))
+sum +=number15
+print(sum)
+number16 =int(input('세 번째 정수형 숫자를 입력해주세요 >'))
+sum +=number16
+print(sum)
+number17 =int(input('네 번째 정수형 숫자를 입력해주세요 >'))
+sum +=number17
+print(sum)
+number18 =int(input('다섯 번째 정수형 숫자를 입력해주세요 >'))
+sum +=number18
+print(sum)
